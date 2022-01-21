5fe3e2e7d721e157b20f3e0bf8ffff4f90e62673
洛水天依 <2461082216@qq.com>
2022-01-21 17:07:42 +0800

    fix: 优化加载文件功能，新增加载文件尾椎过滤

--- src/implement/annotation/implement/initializer/FileSystem.php
+++ src/implement/annotation/implement/initializer/FileSystem.php
@@ -2,19 +2,23 @@
 
 namespace iflow\Container\implement\annotation\implement\initializer;
 
+use FilesystemIterator;
+
 class FileSystem {
 
-    public string $ext = '';
+    public function __construct(protected string|array $ext = '') {
+        $this->ext = is_string($this->ext) ? explode('|', $this->ext) : $this->ext;
+    }
 
     /**
      * 获取文件夹文件
      * @param string $dir
-     * @param string $ext
+     * @param string|array $ext
      * @param bool $traverse
      * @return array
      */
-    public function loadFileList(string $dir, string $ext = '', bool $traverse = false) : array {
-        $this->ext = $ext ?: $this->ext;
+    public function loadFileList(string $dir, string|array $ext = '', bool $traverse = false) : array {
+        $this->ext = $ext ? (is_string($ext) ? explode('|', $ext) : $ext) : $this->ext;
         if (!$traverse) return glob($dir . '*' . $this->ext);
         return match (is_dir($dir)) {
             true => $this->loadDirFile($dir),
@@ -30,12 +34,12 @@ class FileSystem {
      * @return array
      */
     public function loadDirFile(string $dir, array $fileList = []) : array {
-        $iterator = new \FilesystemIterator($dir. DIRECTORY_SEPARATOR);
+        $iterator = new FilesystemIterator($dir. DIRECTORY_SEPARATOR);
         foreach ($iterator as $file) {
             if (is_dir($file -> getPathname())) {
                 $fileList[$file -> getBasename()] = $this->loadDirFile($file -> getPathname());
             } else {
-                if (pathinfo($file -> getExtension()) !== $this->ext) continue;
+                if (!in_array('.'.$file -> getExtension(), $this->ext)) continue;
                 $fileList[str_replace($this->ext, '', $file -> getBasename())] = $file -> getPathname();
             }
         }
